c53c04dc040a20297ce099b217ecfb01a2b0e153
Lucas Gobaco <lucas@gobaco.com>
2024-03-09 17:47:54 -0800

    lucas changes

---
+++ cs124/LucasGobacoWeek6InClassAssignment1/LucasGobacoWeek6InClassAssignment1/ArrayList.cpp
@@ -0,0 +1,168 @@
+/*******************************************************
+
+ * Program Name: ArrayList.cpp
+
+ * Author: Lucas Gobaco
+
+ * Date: 1 March 2024
+
+ * Description: This program implements a class that manages an array list.
+
+ *******************************************************/
+
+#include "ArrayList.h"
+
+using namespace std;
+
+template <typename T>
+ArrayList<T>::ArrayList(int size) {
+    if (size <= 0) {
+        cout << "The array size must be positive. Creating "
+            << "an array of the size 100. " << endl;
+        maxLength = DEFAULT_ARRAY_SIZE;
+    }
+    else
+        maxLength = size;
+
+    length = 0;
+
+    list = new T[maxLength];
+}
+
+template <typename T>
+ArrayList<T>::~ArrayList() {
+    delete[] list;
+}
+
+template <typename T>
+bool ArrayList<T>::isEmpty() const {
+    return (length == 0);
+}
+
+template <typename T>
+bool ArrayList<T>::isFull() const {
+    return (length == maxLength);
+}
+
+template <typename T>
+int ArrayList<T>::size() const {
+    return length;
+}
+
+template <typename T>
+int ArrayList<T>::maxSize() const {
+    return maxLength;
+}
+
+template <typename T>
+void ArrayList<T>::print() const {
+    for (int i = 0; i < length; i++) {
+        cout << list[i] << " ";
+    }
+    cout << endl;
+}
+
+template <typename T>
+bool ArrayList<T>::equal(int location, const T& item)  const {
+    if (location < 0 || location >= length) {
+        cout << "The location of the item to be removed "
+            << "is out of range." << endl;
+        return false;
+    }
+    else
+        return (list[location] == item);
+}
+
+template <typename T>
+void ArrayList<T>::removeAt(int location) {
+    if (location < 0 || location >= length) {
+        cout << "The location of the item to be removed "
+            << "is out of range." << endl;
+    }
+    else {
+        for (int i = location; i < length - 1; i++) {
+            list[i] = list[i + 1];
+        }
+
+        length--;
+    }
+} //end removeAt
+
+template <typename T>
+void ArrayList<T>::retrieveAt(int location, T& retItem) const {
+    if (location < 0 || location >= length) {
+        cout << "The location of the item to be retrieved is "
+                << "out of range" << endl;
+    }
+    else
+        retItem = list[location];
+} //end retrieveAt
+
+template <typename T>
+void ArrayList<T>::clear() {
+    length = 0;
+} //end clear
+
+template <typename T>
+const ArrayList<T>& ArrayList<T>::operator= (const ArrayList<T>& otherList) {
+    if (this != &otherList) {   //avoid self-assignment
+        delete[] list;
+        maxLength = otherList.maxLength;
+        length = otherList.length;
+
+        list = new T[maxLength];
+
+        for (int i = 0; i < length; i++) {
+            list[i] = otherList.list[i];
+        }
+    }
+    return *this;
+}
+
+template <typename T>
+void ArrayList<T>::swap(int first, int second) {
+    T temp;
+
+    temp = list[first];
+    list[first] = list[second];
+    list[second] = temp;
+} //end swap
+
+template <typename T>
+int ArrayList<T>::partition(int first, int last) {
+    T pivot;
+
+    int index, smallIndex;
+
+    swap(first, (first + last) / 2);
+
+    pivot = list[first];
+    smallIndex = first;
+
+    for (index = first + 1; index <= last; index++)
+        if (list[index] < pivot)
+        {
+            smallIndex++;
+            swap(smallIndex, index);
+        }
+
+    swap(first, smallIndex);
+
+    return smallIndex;
+} //end partition
+
+template <typename T>
+void ArrayList<T>::recQuickSort(int first, int last) {
+    int pivotLocation;
+
+    if (first < last) {
+        pivotLocation = partition(first, last);
+        recQuickSort(first, pivotLocation - 1);
+        recQuickSort(pivotLocation + 1, last);
+    }
+} //end recQuickSort
+
+template <typename T>
+void ArrayList<T>::quickSort(int length) {
+    recQuickSort(0, length - 1);
+} //end quickSort
---
+++ cs124/LucasGobacoWeek6InClassAssignment1/LucasGobacoWeek6InClassAssignment1/ArrayList.h
@@ -0,0 +1,140 @@
+/*******************************************************
+
+ * Program Name: ArrayList.h
+
+ * Author: Lucas Gobaco
+
+ * Date: 1 March 2024
+
+ * Description: This program defines a class that manages an array list.
+
+ *******************************************************/
+
+#ifndef H_ARRAYLISTTYPE
+#define H_ARRAYLISTTYPE
+
+#include <iostream>
+
+const int DEFAULT_ARRAY_SIZE = 100;
+
+using namespace std;
+
+template <typename T>
+class ArrayList {
+public:
+    //Constructor
+    //Creates an array of the size specified by the 
+    //parameter size. The default array size is 100.
+    //Postcondition: The list points to the array, length = 0,
+    //               and maxLength = size;
+    ArrayList(int size = DEFAULT_ARRAY_SIZE);
+
+    //Destructor
+    //Deallocate the memory occupied by the array.
+    virtual ~ArrayList();
+
+    //Function to determine whether the list is empty
+    //Postcondition: Returns true if the list is empty;
+    //               otherwise, returns false.
+    bool isEmpty() const;
+
+    //Function to determine whether the list is full
+    //Postcondition: Returns true if the list is full; 
+    //               otherwise, returns false.
+    bool isFull() const;
+
+    //Function to determine the number of elements in 
+    //the list.
+    //Postcondition: Returns the value of length.
+    int size() const;
+
+    //Function to determine the maximum size of the list
+    //Postcondition: Returns the value of maxLength.
+    int maxSize() const;
+
+    //Function to output the elements of the list
+    //Postcondition: Elements of the list are output on the 
+    //               standard output device.
+    void print() const;
+
+    //Function to determine whether item is the same as
+    //the item in the list at the position specified 
+    //by location.
+    //Postcondition: Returns true if the list[location] 
+    //               is the same as item; otherwise, 
+    //               returns false.
+    //               If location is out of range, an 
+    //               appropriate message is displayed.
+    bool equal(int location, const T& item) const;
+
+    //Function to insert insertItem an item at the end of 
+    //the list. Note that this is an abstract function.
+    //Postcondition: list[length] = insertItem; and length++;
+    //               If the list is full, an appropriate 
+    //               message is displayed.
+    virtual void insert(const T& insertItem) = 0;
+
+    //Function to remove the item from the list at the 
+    //position specified by location 
+    //Postcondition: The list element at list[location] is 
+    //               removed and length is decremented by 1.
+    //               If location is out of range, an 
+    //               appropriate message is displayed.
+    void removeAt(int location);
+
+    //Function to retrieve the element from the list at the  
+    //position specified by location 
+    //Postcondition: retItem = list[location] 
+    //               If location is out of range, an 
+    //               appropriate message is displayed.
+    void retrieveAt(int location, T& retItem) const;
+
+    //Function to replace repItem the elements in the list 
+    //at the position specified by location. 
+    //Note that this is an abstract function.
+    //Postcondition: list[location] = repItem 
+    //               If location is out of range, an 
+    //               appropriate message is displayed.
+    virtual void replaceAt(int location, const T& repItem) = 0;
+
+    //Function to remove all the elements from the list 
+    //After this operation, the size of the list is zero.
+    //Postcondition: length = 0;
+    void clear();
+
+    //Function to search the list for searchItem.
+    //Note that this is an abstract function.
+    //Postcondition: If the item is found, returns the 
+    //               location in the array where the item is  
+    //               found; otherwise, returns -1.
+    virtual int search(const T& searchItem) const = 0;
+
+    //Function to remove removeItem from the list.
+    //Note that this is an abstract function.
+    //Postcondition: If removeItem is found in the list,
+    //               it is removed from the list and length 
+    //               is decremented by one.
+    virtual void remove(const T& removeItem) = 0;
+
+    //Function that swaps element at index first and index second
+    void swap(int first, int second);
+
+    //Partitions the list into two halves with the first half's elements all being less than a pivot element and the second half's elements all being greater and returns the index of the pivot
+    int partition(int first, int last);
+
+    //Recursively sorts the list using the quick sort algorithm
+    void recQuickSort(int first, int last);
+    
+    //Sorts the list using the quick sort algorithm
+    void quickSort(int length);
+
+    //Overloads the assignment operator
+    const ArrayList<T>& operator=(const ArrayList<T>&);
+
+protected:
+    T* list;            //array to hold the list elements
+    int length;         //variable to store the length of the list
+    int maxLength;      //variable to store the maximum size of the list
+};
+
+#endif
