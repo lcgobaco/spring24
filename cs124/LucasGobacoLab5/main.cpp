03b6e4cd9041d981d517c3f9f19037367b4da9f8
Michael Gobaco <michael.gobaco@pluto.tv>
2024-05-14 08:30:30 -0700

    clean code

--- cs124/LucasGobacoLab5/main.cpp
+++ cs124/LucasGobacoLab5/main.cpp
@@ -18,30 +18,30 @@
 #include <map>
 
 using namespace std;
-
-map<int, GradeScale*> loadGradeScales(map<string, Section*> sectionMap) {
+map<string, Faculty*> loadFaculties() {
     ifstream inFile;
-    inFile.open("grade-scale-data.csv");
+    inFile.open("faculty-data.csv");
     if (!inFile) {
-        cerr << "Unable to open file grade-scale-data.csv";
+        cerr << "Unable to open file faculty-data.csv";
         exit(1);   // call system to stop
     }
 
-    map<int, GradeScale*> gradeScales;
+    map<string, Faculty*> faculties;
     string line;
     getline(inFile, line);
 
     while (getline(inFile, line)) {
+        if (!line.empty() && line[line.size() - 1] == '\r')
+            line.erase(line.size() - 1);
         vector<string> tokens = splitString(line, ',');
-        Section* section = sectionMap[tokens[1]];
-        GradeScale* gradeScale = new GradeScale(stoi(tokens[0]), section, tokens[2], stod(tokens[3]));
-        gradeScales[stoi(tokens[0])] = gradeScale;
-        section->addGradeScale(gradeScale);
+
+        Faculty* faculty = new Faculty(tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6], tokens[7], tokens[8], tokens[9]);
+        faculties[tokens[0]] = faculty;
     }
 
-    return gradeScales;
-
+    return faculties;
 }
+
 
 map<string, Section*> loadSections(map<string, Faculty*> facultyMap) {
     ifstream inFile;
@@ -69,31 +69,32 @@ map<string, Section*> loadSections(map<string, Faculty*> facultyMap) {
 
     return sections;
 }
-map<string, Faculty*> loadFaculties() {
+
+map<int, GradeScale*> loadGradeScales(map<string, Section*> sectionMap) {
     ifstream inFile;
-    inFile.open("faculty-data.csv");
+    inFile.open("grade-scale-data.csv");
     if (!inFile) {
-        cerr << "Unable to open file faculty-data.csv";
+        cerr << "Unable to open file grade-scale-data.csv";
         exit(1);   // call system to stop
     }
 
-    map<string, Faculty*> faculties;
+    map<int, GradeScale*> gradeScales;
     string line;
     getline(inFile, line);
 
     while (getline(inFile, line)) {
-        if (!line.empty() && line[line.size() - 1] == '\r')
-            line.erase(line.size() - 1);
         vector<string> tokens = splitString(line, ',');
-
-        //Faculty faculty = new Faculty;
-        Faculty* faculty = new Faculty(tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6], tokens[7], tokens[8], tokens[9]);
-        //faculties.insert(std::make_pair(tokens[0],&faculty));
-        faculties[tokens[0]] = faculty;
+        Section* section = sectionMap[tokens[1]];
+        GradeScale* gradeScale = new GradeScale(stoi(tokens[0]), section, tokens[2], stod(tokens[3]));
+        gradeScales[stoi(tokens[0])] = gradeScale;
+        section->addGradeScale(gradeScale);
     }
 
-    return faculties;
+    return gradeScales;
+
 }
+
+
 map<int, Assignment*> loadAssignments(map<int, GradeScale*> gradeScaleMap) {
     ifstream inFile;
     inFile.open("assignment-data.csv");
